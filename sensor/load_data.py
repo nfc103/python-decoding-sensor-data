16a8040bfc2ffafe3eaa57834d06ef592e406250
nfc103 <nunofcarneiro@gmail.com>
2021-11-30 18:31:54 +0000

    Correction to dictionary generation

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -8,4 +8,4 @@ def load_sensor_data():
 
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
-            data_reader = csv.DictReader(data_file,",")
+            data_reader = csv.DictReader(data_file,delimiter=',')
