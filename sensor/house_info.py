0bd8f68de13efdb67d34a85a1b794939f55b2e2b
nfc103 <nunofcarneiro@gmail.com>
2021-11-30 18:58:17 +0000

    Adding method get_data_by_area

---
+++ sensor/house_info.py
@@ -0,0 +1,14 @@
+class HouseInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area=0):
+        field_data =[]
+        for record in self.data:
+            if rec_area==0:
+                field_data.append(record[field])
+            else:
+                if rec_area==int(record['area']):
+                    field_data.append(record[field])
+
+        return field_data
