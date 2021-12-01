f5ee810e2bf470a745a3644e281c468355b9a810
nfc103 <nunofcarneiro@gmail.com>
2021-12-01 17:25:13 +0000

    New method get_data_by_area

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -9,3 +9,7 @@ class TemperatureData(HouseInfo):
                 recs.append(int(rec,base=10))
 
             return recs
+
+        def get_data_by_area(self,rec_area=0):
+            recs = super.get_data_by_area("temperature", rec_area)
+            return _convert_data(recs)
