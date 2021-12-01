7941449dcbc890b709fda4acdbce124bdebee1a3
nfc103 <nunofcarneiro@gmail.com>
2021-12-01 17:40:48 +0000

    Correction

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -11,5 +11,5 @@ class TemperatureData(HouseInfo):
             return recs
 
         def get_data_by_area(self,rec_area=0):
-            recs = super.get_data_by_area("temperature", rec_area)
+            recs = super().get_data_by_area("temperature", rec_area)
             return self._convert_data(recs)
